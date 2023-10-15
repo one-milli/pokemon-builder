d95228a03e0304ab8b753c6e7f2167534e8da923
ohara <kzAme79@gmail.com>
2023-10-16 04:16:47 +0900

    リファクタリング

--- src/components/HpBar.vue
+++ src/components/HpBar.vue
@@ -9,22 +9,16 @@ const props = defineProps({
     defenderStatusRank: Object,
 })
 
-const attacker = ref(props.attacker)
-const defender = ref(props.defender)
-const move = ref(props.move)
-const attackerStatusRank = ref(props.attackerStatusRank)
-const defenderStatusRank = ref(props.defenderStatusRank)
-
 const attackerAttack = computed(() => {
-    return attacker.value.status[1].base * (attackerStatusRank.value.A + 2) / 2
+    return props.attacker.status[1].base * (props.attackerStatusRank.A + 2) / 2
 })
 const defenderDefense = computed(() => {
-    return defender.value.status[2].base * (defenderStatusRank.value.D + 2) / 2
+    return props.defender.status[2].base * (props.defenderStatusRank.D + 2) / 2
 })
 
 const maxDamage = computed(() => {
     //fix
-    return Math.floor(Math.floor(Math.floor(attacker.value.level * 2 / 5 + 2) * move.value.power * attackerAttack.value / defenderDefense.value) / 50 + 2)
+    return Math.floor(Math.floor(Math.floor(props.attacker.level * 2 / 5 + 2) * props.move.power * attackerAttack.value / defenderDefense.value) / 50 + 2)
 })
 const minDamage = computed(() => {
     return Math.floor(maxDamage.value * 0.85)
