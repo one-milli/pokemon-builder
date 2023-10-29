cf26a3750eb5e8bccbfbaefeb2bf15938a1a85c4
ohara <kzAme79@gmail.com>
2023-10-30 04:17:40 +0900

    アイテム、性格を日本語化

--- src/components/selector/SelectItem.vue
+++ src/components/selector/SelectItem.vue
@@ -6,19 +6,22 @@ const props = defineProps({
 });
 
 const items = ref([
-    'Choice Band',
-    'Choice Specs',
-    'Choice Scarf',
-    'Life Orb',
-    'Leftovers',
+    { en: 'Choice Band', ja: 'こだわりハチマキ' },
+    { en: 'Choice Specs', ja: 'こだわりメガネ' },
+    { en: 'Choice Scarf', ja: 'こだわりスカーフ' },
+    { en: 'Life Orb', ja: 'いのちのたま' },
+    { en: 'Leftovers', ja: 'たべのこし' },
+    { en: 'Eviolite', ja: 'しんかのきせき' },
+    { en: 'Assault Vest', ja: 'とつげきチョッキ' },
+    { en: 'Expert Belt', ja: 'たつじんのおび' },
 ])
 
 </script>
 
 <template>
     <select v-model="props.pokemon.pokemon.item">
-        <option v-for="(item, index) in items" :key="index" :value="item">
-            {{ item }}
+        <option v-for="(item, index) in items" :key="index" :value="item.en">
+            {{ item.ja }}
         </option>
     </select>
 </template>
--- src/components/selector/SelectNature.vue
+++ src/components/selector/SelectNature.vue
@@ -6,35 +6,35 @@ const props = defineProps({
 })
 
 const natures = ref([
-    'lonely',
-    'adamant',
-    'naughty',
-    'brave',
-    'bold',
-    'impish',
-    'lax',
-    'relaxed',
-    'modest',
-    'mild',
-    'rash',
-    'quiet',
-    'calm',
-    'gentle',
-    'careful',
-    'sassy',
-    'timid',
-    'hasty',
-    'jolly',
-    'naive',
-    'serious',
+    { en: 'lonely', ja: 'さみしがり' },
+    { en: 'adamant', ja: 'いじっぱり' },
+    { en: 'naughty', ja: 'やんちゃ' },
+    { en: 'brave', ja: 'ゆうかん' },
+    { en: 'bold', ja: 'ずぶとい' },
+    { en: 'impish', ja: 'わんぱく' },
+    { en: 'lax', ja: 'のうてんき' },
+    { en: 'relaxed', ja: 'のんき' },
+    { en: 'modest', ja: 'ひかえめ' },
+    { en: 'mild', ja: 'おっとり' },
+    { en: 'rash', ja: 'うっかりや' },
+    { en: 'quiet', ja: 'れいせい' },
+    { en: 'calm', ja: 'おだやか' },
+    { en: 'gentle', ja: 'おとなしい' },
+    { en: 'careful', ja: 'しんちょう' },
+    { en: 'sassy', ja: 'なまいき' },
+    { en: 'timid', ja: 'おくびょう' },
+    { en: 'hasty', ja: 'せっかち' },
+    { en: 'jolly', ja: 'ようき' },
+    { en: 'naive', ja: 'むじゃき' },
+    { en: 'serious', ja: 'まじめ' },
 ])
 
 </script>
 
 <template>
     <select v-model="props.pokemon.pokemon.nature">
-        <option v-for="(nature, index) in natures" :key="index" :value="nature">
-            {{ nature }}
+        <option v-for="(nature, index) in natures" :key="index" :value="nature.en">
+            {{ nature.ja }}
         </option>
     </select>
 </template>
